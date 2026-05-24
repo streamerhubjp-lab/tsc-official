cb6a431b4aa6c9eb024b7a9ed0da0f3b283db0f4
streamerhubjp-lab <streamerhubjp@gmail.com>
2026-05-25 04:52:00 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', 
+  // 🌟 ここを追加！「このサイトは tsc-official という部屋の中にありますよ」と教える
+  basePath: '/tsc-official', 
   images: {
     unoptimized: true, 
   },
