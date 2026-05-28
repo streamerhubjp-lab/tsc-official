dfcd97fb4a4f3e5d35fe58cb2dba4f4b9d6dc35c
streamerhubjp-lab <streamerhubjp@gmail.com>
2026-05-28 15:42:25 +0900

    Update UltimateCommunitySite.tsx

--- app/UltimateCommunitySite.tsx
+++ app/UltimateCommunitySite.tsx
@@ -511,15 +511,8 @@ return (
                     </div>
                   </motion.div>
                 </section>
-              </>
-            )}
-          </motion.main>
-        </AnimatePresence>
-      </div>
-    </div>
-  );
 
-            {/* ⭐ NEWS & MAGAZINE セクション（2カラムレイアウト） ⭐ */}
+                {/* ⭐ NEWS & MAGAZINE セクション（2カラムレイアウト） ⭐ */}
                 <section className="py-24 bg-white relative z-20">
                   <div className="max-w-6xl mx-auto px-6">
                     <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 lg:gap-24">
@@ -1125,9 +1118,7 @@ return (
                     </div>
                   </section>
               </>
-
-              
-            )}
+              )}
 
             {activePage === 'news' && (
               <section className="pt-32 md:pt-40 pb-32 px-4 md:px-6 bg-[#FAFAFA] min-h-screen relative overflow-hidden">
