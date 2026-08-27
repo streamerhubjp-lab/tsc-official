5680b423f8debcf3760766fd6117a71a92e56c81
streamerhubjp-lab <streamerhubjp@gmail.com>
2026-08-28 06:55:22 +0900

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -129,14 +129,24 @@ export const recommendedCreators: CreatorProfile[] = [
 
 export const galleryItems = [
   {
-    id: 1,
+    id: 2,
     image: '/tsc-official/sss.png',
-    date: '2026.03.25',
-    title: '第3回 TSC3000人記念杯',
+    date: '2025.08.30',
+    title: '第1回 TSC3000人記念杯',
     category: 'TOURNAMENT',
     description:
       '2025年8月30日（土）、配信者やクリエイターが集うDiscordサーバー「The streamer Creator server」の参加者3,000人突破を記念した、特別なAPEXカスタムマッチが開催されました！サーバー設立以来の大きな節目を祝うべく行われた本大会。総勢60名の配信者が参戦し、豪華な演出と驚愕のプレゼント企画で、始終熱気に包まれた当日の様子をレポートをお届けします。',
     articleId: 'activity-001',
+  },
+   {
+    id: 1,
+    image: '/tsc-official/4000playsamune.PNG',
+    date: '2026.05.24',
+    title: '参加者4000人突破記念！「TSC APEXカスタム 予選」2日間の激闘をプレイバック！',
+    category: 'EVENT',
+    description:
+      '2026年5月23日（土）・24日（日）の2日間にわたり、配信者やクリエイターが集う大人気Discordサーバー「The streamer Creator server」の参加者4,000人突破を記念したAPEXカスタム予選（Aブロック・Bブロック）が開催されました！',
+    articleId: 'activity-002',
   },
 ];
 
@@ -285,7 +295,7 @@ export const articleItems = [
   {
     id: 'activity-001',
     category: 'TOURNAMENT',
-    date: '2026.03.25',
+    date: '2025.08.30',
     title: '第1回 TSC3000人記念杯 開催レポート！',
     thumbnail: '/tsc-official/sss.png',
     content: `2025年8月30日（土）、配信者やクリエイターが集うDiscordサーバー「The streamer Creator server」の参加者<span class="text-blue-600 font-black text-xl md:text-2xl">3,000人突破</span>を記念した、特別なAPEXカスタムマッチが開催されました！<br><br>
