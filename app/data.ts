eb8442b19df14fceec063f63af917ebcd5a7849b
streamerhubjp-lab <streamerhubjp@gmail.com>
2026-08-28 06:12:05 +0900

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -130,16 +130,6 @@ export const recommendedCreators: CreatorProfile[] = [
 export const galleryItems = [
   {
     id: 1,
-    image: '/tsc-official/tsc-official/sss.png',
-    date: '2026.03.25',
-    title: '第3回 TSC3000人記念杯',
-    category: 'TOURNAMENT',
-    description:
-      '2025年8月30日（土）、配信者やクリエイターが集うDiscordサーバー「The streamer Creator server」の参加者3,000人突破を記念した、特別なAPEXカスタムマッチが開催されました！サーバー設立以来の大きな節目を祝うべく行われた本大会。総勢60名の配信者が参戦し、豪華な演出と驚愕のプレゼント企画で、始終熱気に包まれた当日の様子をレポートをお届けします。',
-    articleId: 'activity-001',
-  },
-    {
-    id: 1,
     image: '/tsc-official/sss.png',
     date: '2026.03.25',
     title: '第3回 TSC3000人記念杯',
